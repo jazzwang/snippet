2f574ec4954d0963527198401318f6379c4ac056
jazzwang <jazzwang.tw@gmail.com>
2016-06-01 18:37:53 +0800

    sample code to collect product catagory

--- src/main/java/ParseLinksExample.java
+++ src/main/java/ParseLinksExample.java
@@ -13,23 +13,28 @@ public class ParseLinksExample {
     Document doc;
     try {
       Map<String, String> cookies = null;
-      String url = "http://goods.ruten.com.tw/item/show?21621768431742";
+      String url = "http://goods.ruten.com.tw/item/show?" + args[0];
+      System.out.println(url);
       Response res = Jsoup.connect(url).userAgent("Mozilla").followRedirects(false).execute();
+      //System.out.println("url:"+res.url());
+      //System.out.println("header:"+res.header("location"));
+      //System.out.println("cookie:"+cookies);
       res = Jsoup.connect(res.header("location")).userAgent("Mozilla").followRedirects(false).execute();
       cookies = res.cookies();
-      System.out.println("url:"+res.url());
-      System.out.println("header:"+res.header("location"));
-      System.out.println("cookie:"+cookies);
-      doc = Jsoup.connect(url).cookies(cookies).timeout(30000).get();
+      //System.out.println("url:"+res.url());
+      //System.out.println("header:"+res.header("location"));
+      //System.out.println("cookie:"+cookies);
+      doc = Jsoup.connect(url).cookies(cookies).timeout(300).get();
       // get title of the page
       String title = doc.title();
       System.out.println("Title: " + title);
+      // System.out.println(doc.body());
       // get all links
-      Elements links = doc.select("a[href]");
+      Elements links = doc.select(".rt-breadcrumb-link");
       for (Element link : links) {
         // get the value from href attribute
-        System.out.println("\nLink : " + link.attr("href"));
-        System.out.println("Text : " + link.text());
+        if(link.text() != "")
+          System.out.println(link.attr("href")+","+link.text());
       }
     } catch (IOException e) {
       e.printStackTrace();
