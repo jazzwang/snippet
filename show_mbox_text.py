baf7df2912450d3a7a6815a32f548c8f18bc084e
jazzwang <jazzwang.tw@gmail.com>
2018-06-10 10:26:07 +0800

    initial python script to parse mbox file

---
+++ show_mbox_text.py
@@ -0,0 +1,10 @@
+import mailbox
+
+mbox = mailbox.mbox('Indeed.mbox')
+for message in mbox:
+	if message.is_multipart():
+		content = ''.join(part.get_payload(decode=True) for part in message.get_payload())
+	else:
+		content = message.get_payload(decode=True)
+	print message['Date']
+	print content
