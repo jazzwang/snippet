0f8cb10a46cee3ff057f4a0b7369911b400eb336
Jazz Wang <jazzwang.tw@gmail.com>
2019-04-29 10:29:41 +0800

    [spark-sample-etl-scala] add sample scala code and input json files

--- spark-sample-etl-scala/build.sbt
+++ spark-sample-etl-scala/build.sbt
@@ -1,14 +1,17 @@
 import Dependencies._
 
-ThisBuild / scalaVersion     := "2.12.8"
-ThisBuild / version          := "0.1.0-SNAPSHOT"
+ThisBuild / scalaVersion     := "2.11.11"
+ThisBuild / version          := "0.1.0"
 ThisBuild / organization     := "com.example"
 ThisBuild / organizationName := "example"
 
 lazy val root = (project in file("."))
   .settings(
     name := "spark-sample-etl-scala",
-    libraryDependencies += scalaTest % Test
+    libraryDependencies ++= Seq(
+      "org.apache.spark"  %%  "spark-core"    % "2.2.1",
+      "org.apache.spark"  %%  "spark-sql"     % "2.2.1"
+    )
   )
 
 // See https://www.scala-sbt.org/1.x/docs/Using-Sonatype.html for instructions on how to publish to Sonatype.
--- spark-sample-etl-scala/src/main/scala/example/Hello.scala
+++ spark-sample-etl-scala/src/main/scala/example/Hello.scala
@@ -1,7 +1,30 @@
 package example
 
+import org.apache.spark.sql.SparkSession
+
 object Hello extends Greeting with App {
   println(greeting)
+
+  val whereami = System.getProperty("user.dir")
+  println(whereami)
+
+  var spark = SparkSession.builder
+    .appName("Spark Example")
+    .master("local[*]")
+    .getOrCreate
+
+  var employeeDF = spark.read.json("input/employees.json")
+  employeeDF.printSchema()
+  employeeDF.createOrReplaceTempView("employee")
+  
+  var peopleDF = spark.read.json("input/people.json")
+  peopleDF.printSchema()
+  peopleDF.createOrReplaceTempView("people")
+
+  var outputDF = spark.sql("select employee.name, people.age, employee.salary from employee, people where employee.name = people.name order by salary")
+  outputDF.show()
+  outputDF.write.parquet("output")
+  spark.stop()
 }
 
 trait Greeting {
