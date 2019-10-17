dd5bfc9cfad26e2ba3aebb84e9be2ccb41ddc44b
jazzwang <jazzwang.tw@gmail.com>
2019-10-17 22:51:11 +0800

    read CSV into Spark DataFrame and try to insert to SQLite

--- liquibase-scala/src/main/scala/example/Hello.scala
+++ liquibase-scala/src/main/scala/example/Hello.scala
@@ -1,7 +1,29 @@
 package example
 
+import org.apache.spark.sql.SparkSession
+import java.sql._
+
 object Hello extends Greeting with App {
   println(greeting)
+
+  val whereami = System.getProperty("user.dir")
+  println(whereami)
+
+  var spark = SparkSession.builder
+    .appName("Spark Example")
+    .master("local[*]")
+//  .config("spark.eventLog.enabled","true")
+    .getOrCreate
+
+  var input = spark.read.csv("input/sample.csv")
+  input.printSchema()
+
+  val connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+  val statement = connection.createStatement();
+
+  connection.close();
+
+  spark.stop()
 }
 
 trait Greeting {
