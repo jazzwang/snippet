b07a4ac58ebc907e5b953ed0fd9a6ad956053401
jazzwang <jazzwang.tw@gmail.com>
2019-11-02 17:23:20 +0800

    add basic.ps1 for cmder

---
+++ basic.ps1
@@ -0,0 +1,12 @@
+mkdir cmder
+cd cmder
+# cmder - https://cmder.net/
+## curl command in PowerShell?
+## https://idiallo.com/blog/download-file-in-windows-command-line
+curl https://github.com/cmderdev/cmder/releases/download/v1.3.12/cmder.zip
+unzip cmder.zip
+del cmder.zip
+cd bin
+# wget - https://eternallybored.org/misc/wget/
+curl https://eternallybored.org/misc/wget/1.20.3/64/wget.exe
+# setx Path 
