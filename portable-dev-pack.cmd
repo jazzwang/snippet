9330314c61146c135031bdc8ef1c83b03f3e1ad5
jazzwang <jazzwang.tw@gmail.com>
2019-11-20 22:58:52 +0800

    add portable-dev-pack.cmd

---
+++ portable-dev-pack.cmd
@@ -0,0 +1,4 @@
+start https://github.com/cmderdev/cmder/releases/download/v1.3.12/cmder.zip
+start https://eternallybored.org/misc/wget/1.20.3/64/wget.exe
+wget https://cdn.azul.com/zulu/bin/zulu8.42.0.23-ca-jdk8.0.232-win_x64.zip
+path %PATH%;"c:\Program Files\7-Zip"
