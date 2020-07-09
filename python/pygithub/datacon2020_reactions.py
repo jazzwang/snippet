35838a4f0a07029dc6a78c760fcf1a113a69ceb2
jazzwang <jazzwang.tw@gmail.com>
2020-07-09 18:05:05 +0800

    [pygithub] add 'issue title' to datacon2020_reactions.csv

--- python/pygithub/datacon2020_reactions.py
+++ python/pygithub/datacon2020_reactions.py
@@ -1,5 +1,3 @@
-#!env python
-
 import os
 from github import Github
 
@@ -17,7 +15,7 @@ g = Github(github_id, github_password)
 ## change to 512 Bytes
 reactions = open('datacon2020_reactions.csv','w+', 512)
 ## Write CSV headers
-print("issue_id,user_id,reaction_type,timestamp",file=reactions)
+print("issue_id,issue_title,user_id,reaction_type,timestamp",file=reactions)
 
 ## https://pygithub.readthedocs.io/en/latest/examples/Repository.html#get-list-of-open-issues
 repo = g.get_repo("datacon2020/proposal")
@@ -34,5 +32,5 @@ for issue in repo.get_issues():
         ## https://pygithub.readthedocs.io/en/latest/github_objects/NamedUser.html#github.NamedUser.NamedUser.name
         ## - reaction.created_at: Type:	datetime.datetime
         ## https://pygithub.readthedocs.io/en/latest/github_objects/Reaction.html#github.Reaction.Reaction.created_at
-        print(str(issue.number) + "," + reaction.user.login + "," + reaction.content + "," + str(reaction.created_at), file=reactions)
-        print(str(issue.number) + "," + reaction.user.login + "," + reaction.content + "," + str(reaction.created_at))
+        print(str(issue.number) + ",\"" + issue.title + "\"," + reaction.user.login + "," + reaction.content + "," + str(reaction.created_at), file=reactions)
+        print(str(issue.number) + ",\"" + issue.title + "\"," + reaction.user.login + "," + reaction.content + "," + str(reaction.created_at))
