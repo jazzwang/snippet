76e5e6867e8dfb37fcbfe182d3d8af144ac3405e
jazzwang <jazzwang.tw@gmail.com>
2021-04-14 00:17:15 +0800

    [scala][nyc-taxi-parquet] modified test.sh

--- scala/nyc-taxi-parquet/test.sh
+++ scala/nyc-taxi-parquet/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-sbt 'run -i nyc-tlc/yellow_v1 -o out/schema=1 -v 1'
-sbt 'run -i nyc-tlc/yellow_v2 -o out/schema=2 -v 2'
-sbt 'run -i nyc-tlc/yellow_v3 -o out/schema=3 -v 3'
-sbt 'run -i nyc-tlc/yellow_v4 -o out/schema=4 -v 4'
-sbt 'run -i nyc-tlc/yellow_v5 -o out/schema=5 -v 5'
+sbt package
+spark-submit --packages "com.github.scopt:scopt_2.12:4.0.0" --class example.NYC2Parquet target/scala-2.12/nyc2parquet_2.12-0.1.0.jar -i nyc-tlc/yellow_v1 -o parquet/v1 -v 1
+spark-submit --packages "com.github.scopt:scopt_2.12:4.0.0" --class example.NYC2Parquet target/scala-2.12/nyc2parquet_2.12-0.1.0.jar -i nyc-tlc/yellow_v2 -o parquet/v2 -v 2
+spark-submit --packages "com.github.scopt:scopt_2.12:4.0.0" --class example.NYC2Parquet target/scala-2.12/nyc2parquet_2.12-0.1.0.jar -i nyc-tlc/yellow_v3 -o parquet/v3 -v 3
+spark-submit --packages "com.github.scopt:scopt_2.12:4.0.0" --class example.NYC2Parquet target/scala-2.12/nyc2parquet_2.12-0.1.0.jar -i nyc-tlc/yellow_v4 -o parquet/v4 -v 4
+spark-submit --packages "com.github.scopt:scopt_2.12:4.0.0" --class example.NYC2Parquet target/scala-2.12/nyc2parquet_2.12-0.1.0.jar -i nyc-tlc/yellow_v5 -o parquet/v5 -v 5
