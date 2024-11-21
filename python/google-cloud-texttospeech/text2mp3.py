3aef24b743c24173a75a573ab8859402973a68e2
jazzwang <jazzwang.tw@gmail.com>
2024-11-21 22:44:25 +0800

    feat: [py][google-cloud-texttospeech] example text-to-mp3 script using Google Cloud Text-to-Speech Client Library

---
+++ python/google-cloud-texttospeech/text2mp3.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+import google.cloud.texttospeech as tts
+
+def text_to_mp3(voice_name: str, text: str):
+    language_code = "-".join(voice_name.split("-")[:2])
+    text_input = tts.SynthesisInput(text=text)
+    voice_params = tts.VoiceSelectionParams(
+        language_code=language_code, name=voice_name
+    )
+    audio_config = tts.AudioConfig(audio_encoding=tts.AudioEncoding.MP3)
+    client = tts.TextToSpeechClient()
+    response = client.synthesize_speech(
+        input=text_input,
+        voice=voice_params,
+        audio_config=audio_config,
+    )
+    filename = f"{voice_name}.mp3"
+    with open(filename, "wb") as out:
+        out.write(response.audio_content)
+        print(f'Generated speech saved to "{filename}"')
+
+text_to_mp3("en-US-Studio-O", "shoemaker s-h-o-e-m-a-k-e-r shoemaker")
