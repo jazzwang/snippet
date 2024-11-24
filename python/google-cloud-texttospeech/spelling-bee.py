7c7be538503ba65a29a7923f2e4d76135e4bcc3e
jazzwang <jazzwang.tw@gmail.com>
2024-11-24 20:05:14 +0800

    feat: [py][google-cloud-texttospeech] enhance: read from input file

--- python/google-cloud-texttospeech/spelling-bee.py
+++ python/google-cloud-texttospeech/spelling-bee.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import google.cloud.texttospeech as tts
+import argparse
 
 def text_to_mp3(voice_name: str, text: str, output_name: str):
     language_code = "-".join(voice_name.split("-")[:2])
@@ -8,8 +9,8 @@ def text_to_mp3(voice_name: str, text: str, output_name: str):
         language_code=language_code, name=voice_name
     )
     audio_config = tts.AudioConfig(
-        audio_encoding=tts.AudioEncoding.MP3,  
-        speaking_rate=0.75,
+        audio_encoding=tts.AudioEncoding.MP3,
+        speaking_rate=0.5,
     )
     client = tts.TextToSpeechClient()
     response = client.synthesize_speech(
@@ -32,6 +33,21 @@ def say_spell_say(word: str):
     spell = word_to_spell(word)
     return f"{word}...{spell}...{word}"
 
-word = "shoemaker"
-print(say_spell_say(word))
-text_to_mp3("en-US-Standard-C",say_spell_say(word), word)
+def spelling_bee(word: str):
+    text_to_mp3("en-US-Studio-O",say_spell_say(word), word)
+
+def main():
+    # parsing CLI arguments
+    parser = argparse.ArgumentParser(description="Generate Spelling Bee .")
+    parser.add_argument("input_file", help="input file with words.")
+    args = parser.parse_args()
+
+    input_file = args.input_file
+
+    with open(input_file, 'r') as f:
+        words = f.readlines()
+        for word in words:
+            spelling_bee(word.strip())
+
+if __name__ == "__main__":
+    main()
