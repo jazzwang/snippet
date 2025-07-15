eb48c24e045ee8e8af9a84137db0d4abc8cad066
jazzwang <jazzwang.tw@gmail.com>
2025-07-15 00:13:27 +0000

    fix: [py][playwright] test-kindle-web.py: read kindle login credential from environment variables.

--- py/playwright/test-kindle-web.py
+++ py/playwright/test-kindle-web.py
@@ -1,6 +1,9 @@
-import re
+import re, os
 from playwright.sync_api import Playwright, sync_playwright, expect
 
+## read LOGIN_MAIL from environment variable
+KINDLE_MAIL = os.environ.get("KINDLE_MAIL")
+KINDLE_PASSWD = os.environ.get("KINDLE_PASSWD")
 
 def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
@@ -8,9 +11,9 @@ def run(playwright: Playwright) -> None:
     page = context.new_page()
     page.goto("https://www.amazon.com/ap/signin?openid.pape.max_auth_age=1209600&openid.return_to=https%3A%2F%2Fread.amazon.com%2F%3Fasin%3DB0DM6BHQYR&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=amzn_kindle_mykindle_us&openid.mode=checkid_setup&language=en_US&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0")
     page.get_by_role("textbox", name="Email or mobile phone number").click()
-    page.get_by_role("textbox", name="Email or mobile phone number").fill("jazz.wang@example.com")
+    page.get_by_role("textbox", name="Email or mobile phone number").fill(KINDLE_MAIL)
     page.get_by_role("button", name="Continue").click()
-    page.get_by_role("textbox", name="Password").fill("THIS-IS-MY-PASSWORD")
+    page.get_by_role("textbox", name="Password").fill(KINDLE_PASSWD)
     page.get_by_role("button", name="Sign in").click()
     page.get_by_role("button", name="Next page").click()
     page.get_by_role("button", name="Next page").click()
